aa7c012a2c595aee732c07ba2d07ffb2425dc3bf
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-05 16:09:03 +0000

    Update src/App.jsx: [Describe the changes. E.g., Modified routing, updated components]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import NotFound from './pages/NotFound'
+import Register from './pages/Register'
+import RegisterLayout from './layouts/RegisterLayout'
+import Confirmation from './pages/Confirmation'
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing/>}/>
       <Route path='*' element={<NotFound/>}/>
+      <Route element={<RegisterLayout/>}>
+        <Route path="/register" element={<Register/>}/>
+        <Route path='/verify' element={<Confirmation/>}/>
+      </Route>
     </Routes>
   )
 }
