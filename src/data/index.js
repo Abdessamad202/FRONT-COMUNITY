2a872da44f5c2bbc390f4439ab7b5f98dac04016
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-05 15:28:47 +0000

    Add src/data/index.js: Initial data file

---
+++ src/data/index.js
@@ -0,0 +1,43 @@
+import { BookOpen, Calendar, MessageCircle, Users } from "lucide-react";
+
+export const cardData = [
+  {
+    icon: Users,
+    title: "Active Members",
+    description: "Connect with passionate individuals from around the world.",
+    img: "http://localhost:8000/imgs/cards/pic1.jpg",
+  },
+  {
+    icon: MessageCircle,
+    title: "Discussions",
+    description: "Engage in meaningful conversations about topics that matter.",
+    img: "http://localhost:8000/imgs/cards/pic2.jpg",
+  },
+  {
+    icon: Calendar,
+    title: "Events",
+    description: "Participate in regular meetups, workshops, and conferences.",
+    img: "http://localhost:8000/imgs/cards/pic3.jpg",
+    },
+  {
+    icon: BookOpen,
+    title: "Resources",
+    description: "Access a wealth of knowledge and learning materials.",
+    img: "http://localhost:8000/imgs/cards/pic4.jpg",
+  },
+];
+
+export const statsData = [
+  {
+    title: "Active Members",
+    value: "10K+",
+  },
+  {
+    title: "Daily Discussions",
+    value: "5K+",
+  },
+  {
+    title: "Monthly Events",
+    value: "100+",
+  },
+];
