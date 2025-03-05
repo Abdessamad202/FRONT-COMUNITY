0a742bf378bc5041c07c38b3e55bf00c167c484d
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-05 16:09:04 +0000

    Update src/main.jsx: [Describe the changes. E.g., Updated entry point, modified rendering]

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./css/index.css";
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UserProvider } from './components/UserProvider.jsx';
+import { NotificationProvider } from './components/NotificationProvider.jsx';
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+        <NotificationProvider>
+          <QueryClientProvider client={queryClient}>
+            <UserProvider>
+              <App />
+            </UserProvider>
+          </QueryClientProvider>
+        </NotificationProvider>
     </BrowserRouter>
 
   </StrictMode>
