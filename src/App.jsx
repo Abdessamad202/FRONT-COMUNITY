2e640f473f5714c5a62c419990116048aeeff3eb
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-05 15:28:39 +0000

    Add src/App.jsx: Initial App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router'
+import Landing from './pages/Landing'
+import NotFound from './pages/NotFound'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
+  )
+}
+
+export default App
