3206134aa675be6d41ce414e4c1487b4445bb1d5
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-29 18:19:49 +0000

    Modified RegisterSteps.jsx

--- src/components/RegisterSteps.jsx
+++ src/components/RegisterSteps.jsx
@@ -1,21 +1,22 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext"; // Import UserContext to access the current user's data
-import { getStepColor } from "../utils/getStepColor"; // Import utility function to get the color based on the step
+import { useLocation } from "react-router"; // Import useLocation to get the current pathname
+import {getStepColor} from '../utils/getStepColor'
+const RegisterSteps = ({ status }) => {
+  const { user } = useContext(UserContext); // Access the user data from context
+  const location = useLocation(); // Get the current path using useLocation()
 
-const RegisterSteps = () => {
-  // Destructure user data from UserContext
-  const { user } = useContext(UserContext);
+  
 
   return (
     <div className="text-center mb-8">
       {/* Step navigation container */}
       <div className="flex justify-around items-center steps">
-
         {/* Step 1 - Register */}
         <div className="flex flex-col items-center space-y-2 step">
           {/* Step circle */}
           <div
-            className={`w-8 h-8 rounded-full ${getStepColor(1, "/register", user?.step || 0)} text-white flex items-center justify-center`}
+            className={`w-8 h-8 rounded-full text-white flex items-center justify-center ${getStepColor(1)}`}
           >
             <span>1</span>
           </div>
@@ -27,7 +28,7 @@ const RegisterSteps = () => {
         <div className="flex flex-col items-center space-y-2 step">
           {/* Step circle */}
           <div
-            className={`w-8 h-8 rounded-full ${getStepColor(2, "/verify", user?.step)} text-white flex items-center justify-center`}
+            className={`w-8 h-8 rounded-full text-white flex items-center justify-center ${getStepColor(2)}`}
           >
             <span>2</span>
           </div>
@@ -39,7 +40,7 @@ const RegisterSteps = () => {
         <div className="flex flex-col items-center space-y-2 step">
           {/* Step circle */}
           <div
-            className={`w-8 h-8 rounded-full ${getStepColor(3, "/profile-completion", user?.step)} text-white flex items-center justify-center`}
+            className={`w-8 h-8 rounded-full text-white flex items-center justify-center ${getStepColor(3)}`}
           >
             <span>3</span>
           </div>
