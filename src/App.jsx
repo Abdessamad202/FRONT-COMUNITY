e495a58869f1f426ef4383d8094170b73515c909
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-29 18:19:45 +0000

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import Landing from './pages/Landing'
 import NotFound from './pages/NotFound'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
-import Confirmation from './pages/Confirmation'
+import Verification from './pages/Verification'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import CheckMail from './pages/CheckMail'
+import ValidateCode from './pages/ValidateCode'
+import ChangePassword from './pages/ChangePassword'
+import HomePage from './pages/Home'
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing/>}/>
-      <Route path='*' element={<NotFound/>}/>
-      <Route element={<RegisterLayout/>}>
-        <Route path="/register" element={<Register/>}/>
-        <Route path='/verify' element={<Confirmation/>}/>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path='*' element={<NotFound />} />
+      <Route element={<RegisterLayout />}>
+        <Route path="/register" element={<Register />} />
+        <Route path='/verify-email' element={<Verification />} />
+        <Route path='/complete-profile' element={<Profile />} />
       </Route>
+      <Route path='/check-email' element={<CheckMail/>} />
+      <Route path='/validate-code' element={<ValidateCode />}/>
+      <Route path='/change-password' element={<ChangePassword />}/>
+      <Route path='/home' element={<HomePage />} />
     </Routes>
   )
 }
