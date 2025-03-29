acef40f8145964f5023c22c3703efca6525d1d30
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-29 18:19:46 +0000

    Modified axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://backend_comunity_app.test/api",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
