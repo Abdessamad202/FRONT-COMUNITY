751a17df10b89f2f004d3320e23132eafeb3c66d
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-29 18:19:56 +0000

    Added getStepColor.js

--- src/utils/getStepColor.js
+++ src/utils/getStepColor.js
@@ -1,5 +1,21 @@
-export const getStepColor = (stepNumber, path, actualStep) => {
-  if (actualStep >= stepNumber) return "bg-green-600"; // Completed
-  else if (location.pathname === path) return "bg-indigo-600"; // In progress
-  return "bg-gray-300"; // Not started
+// Helper function to determine step color based on the current path and status
+export const getStepColor = (stepNumber) => {
+  if (stepNumber === 1) {
+    // Step 1 - Register
+    return location.pathname === "/register" ? "bg-indigo-600" : "bg-green-600";
+  } else if (stepNumber === 2) {
+    // Step 2 - Verify Email
+    return location.pathname === "/verify-email"
+      ? "bg-indigo-600"
+      : location.pathname === "/complete-profile" 
+      ? "bg-green-600"
+      : "bg-gray-300";
+  } else if (stepNumber === 3) {
+    // Step 3 - Complete Profile
+    return location.pathname === "/complete-profile"
+      ? "bg-indigo-600"
+      : location.pathname === "/verify-email" || location.pathname === "/register"
+      ? "bg-gray-300"
+      : "bg-green-600"
+  }
 };
